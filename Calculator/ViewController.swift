f5df10d4aca92e56f3de620d23cd83cf2c602550
Nikita Hans <nikita.s.hans@googlemail.com>
2018-03-17 16:32:14 +0100

    Uploading Calculator Project

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,124 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CalculatorViewController: UIViewController {
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultTextField: UITextField!
+    @IBOutlet weak var termTextField: UITextField!
+    
+    private let numPadToolBar = UIToolbar()
+    private var calculatorViewModel: CalculatorViewModelProtocol?
+    private var needsReset = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        calculatorViewModel = CalculatorViewModel()
+        resultLabel.text = "Calculator"
+        resultTextField.isEnabled = false
+        createDecimalPad()
+    }
+    
+    private func createDecimalPad() {
+        termTextField.keyboardType = .decimalPad
+        setUpNumPadToolBar()
+        termTextField.inputAccessoryView = numPadToolBar
+        termTextField.becomeFirstResponder()
+    }
+    
+    private func setUpNumPadToolBar() {
+        numPadToolBar.sizeToFit()
+        numPadToolBar.items = [
+            UIBarButtonItem(title: buttonLabel(.clear), style: .done, target: self, action: #selector(clear)),
+            UIBarButtonItem(title: buttonLabel(.plus), style: .plain, target: self, action: #selector(plus)),
+            UIBarButtonItem(title: buttonLabel(.minus), style: .plain, target: self, action: #selector(minus)),
+            UIBarButtonItem(title: buttonLabel(.multiply), style: .plain, target: self, action: #selector(multiply)),
+            UIBarButtonItem(title: buttonLabel(.divide), style: .plain, target: self, action: #selector(divide)),
+            UIBarButtonItem(title: buttonLabel(.calculate), style: .done, target: self, action: #selector(calc))
+        ]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: User Button Functions
+    @objc func clear() {
+        termTextField.text = ""
+        resultTextField.text = ""
+        guard let viewModel = calculatorViewModel else { return }
+        viewModel.clear()
+        needsReset = false
+    }
+    
+    @objc func plus() {
+        guard let viewModel = calculatorViewModel else { return }
+        viewModel.addToTerm(userInput: termTextField.text, mathAction: .plus)
+        updateTextViews(.plus)
+    }
+    
+    @objc func minus() {
+        guard let viewModel = calculatorViewModel else { return }
+        viewModel.addToTerm(userInput: termTextField.text, mathAction: .minus)
+        updateTextViews(.minus)
+    }
+    
+    @objc func multiply() {
+        guard let viewModel = calculatorViewModel else { return }
+        viewModel.addToTerm(userInput: termTextField.text, mathAction: .multiply)
+        updateTextViews(.multiply)
     }
 
+    @objc func divide() {
+        guard let viewModel = calculatorViewModel else { return }
+        viewModel.addToTerm(userInput: termTextField.text, mathAction: .divide)
+        updateTextViews(.divide)
+    }
+    
+    @objc func calc() {
+        guard let viewModel = calculatorViewModel else { return }
+        viewModel.addToTerm(userInput: termTextField.text, mathAction: .calculate)
+        if let prefix = resultTextField.text, let value = termTextField.text {
+            resultTextField.text = "\(prefix) \(value) ="
+        }
+        termTextField.text = "\(viewModel.calculate())"
+        needsReset = true
+    }
+    
+    //MARK: TextViewUpdate
+    private func updateTextViews(_ action: UserAction) {
+        if needsReset {
+            resultTextField.text = ""
+            needsReset = false
+        }
+        guard let text = termTextField.text else { return }
+        termTextField.text = ""
+        if let previous = resultTextField.text, previous != "" {
+            resultTextField.text = "\(previous) \(text) \(symbol(from: action))"
+        } else {
+            resultTextField.text = "\(text) \(symbol(from: action))"
+        }
+    }
+    
+    // MARK: UserAction to String Helper Functions
+    private func symbol(from action: UserAction) -> String {
+        return buttonLabel(action).trimmingCharacters(in: .whitespaces)
+    }
 
+    private func buttonLabel(_ action: UserAction) -> String {
+        let spacing = "     "
+        switch action {
+        case .clear:
+            return "\(spacing)reset"
+        case .plus:
+            return "\(spacing)+"
+        case .minus:
+            return "\(spacing)-"
+        case .multiply:
+            return "\(spacing)x"
+        case .divide:
+            return "\(spacing):"
+        case .calculate:
+            return "\(spacing)="
+        case .error:
+            return "\(spacing)ERROR"
+        }
+    }
 }
+
 
---
+++ Calculator/ViewModel.swift
@@ -0,0 +1,105 @@
+//
+//  ViewModel.swift
+//  Calculator
+//
+//  Created by Nikita Hans on 17.03.18.
+//  Copyright © 2018 Nikita Hans. All rights reserved.
+//
+
+import Foundation
+
+enum UserAction {
+    case clear
+    case plus
+    case minus
+    case divide
+    case multiply
+    case calculate
+    case error
+}
+
+protocol CalculatorViewModelProtocol {
+    //Clears the mathematic and logic term currently saved
+    func clear()
+    //Adds a new Double as a value paired with a mathAction to the term, if String is invalid to be used as a Double the term is invalidated
+    func addToTerm(userInput: String?, mathAction: UserAction)
+    //Computes the current term to a rounded Double value which is returned as a String, the term is cleared after the computation
+    func calculate() -> String
+}
+
+class CalculatorViewModel: CalculatorViewModelProtocol {
+    private var term: [valueOperatorPair] = [valueOperatorPair]()
+    private struct valueOperatorPair {
+        let value: Double
+        var usrActn: UserAction
+    }
+    
+    func clear() {
+        term = [valueOperatorPair]()
+    }
+    
+    func addToTerm(userInput: String?, mathAction: UserAction) {
+        if let validValue = Double(userInput ?? "0") {
+            term.append(valueOperatorPair(value: validValue , usrActn: mathAction))
+        } else {
+            term.append(valueOperatorPair(value: 0.0001, usrActn: .error))
+        }
+    }
+    
+    func calculate() -> String {
+                var lowPriorityTerm = [valueOperatorPair]()
+                var result: Double = 0
+                var prevValue: Double?
+                var nextOp: UserAction = .clear
+                //MARK: high Priority calc
+                for elem in term {
+                    var currentElement = elem
+                    if let previous = prevValue {
+                        if nextOp == .multiply {
+                            currentElement = valueOperatorPair(value: previous * elem.value, usrActn: elem.usrActn)
+                        } else if nextOp == .divide {
+                            if elem.value == 0 {
+                                currentElement = valueOperatorPair(value: previous, usrActn: .error)
+                            } else {
+                                currentElement = valueOperatorPair(value: previous / elem.value, usrActn: elem.usrActn)
+                            }
+                        }
+                        prevValue = nil
+                    }
+                    if currentElement.usrActn == .multiply || currentElement.usrActn == .divide {
+                        prevValue = currentElement.value
+                        nextOp = currentElement.usrActn
+                    } else {
+                        lowPriorityTerm.append(currentElement)
+                    }
+                }
+                //MARK: low Priority calc
+                for (index,elem) in lowPriorityTerm.enumerated() {
+                    if index != 0 {
+                        switch nextOp{
+                        case .plus:
+                            result += elem.value
+                        case .minus:
+                            result -= elem.value
+                        case .divide, .multiply:
+                            print("found high priority  while calculating low priority ops")
+                        case .calculate:
+                            clear()
+                        default:
+                            print("found non math op in low priority ops term")
+                            break
+                        }
+                    } else {
+                        if elem.usrActn == .error {
+                            clear()
+                            term = [valueOperatorPair]()
+                            return "ERROR"
+                        }
+                        result = elem.value
+                    }
+                    nextOp = elem.usrActn
+                }
+                term = [valueOperatorPair]()
+                return ("\(Double(Int(result*1000))/1000)")
+    }
+}
